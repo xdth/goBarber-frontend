b16f846734e46f3137eb0fd1111a1f12258e3572
dth <dth@dthlabs.com>
2020-07-05 20:12:12 +0200

    toast structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -10,6 +11,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
