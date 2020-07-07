d6cd4b996b23e842998df0309127523895ec2dac
dth <dth@dthlabs.com>
2020-07-07 19:52:22 +0200

    setting up routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from "react-router-dom";
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -74,13 +75,13 @@ const SignIn: React.FC = () => {
 
           <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
+          <a href="/forgot">Esqueci minha senha</a>
         </Form>
 
-        <a href="login">
+        <Link to="/signup">
           <FiLogIn />
           Criar conta
-        </a>
+        </Link>
       </Content>
       <Background/>
     </Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationError';
+import { Link } from "react-router-dom";
 
 import logoImg from '../../assets/logo.svg'
 
@@ -56,10 +57,10 @@ const SignUp: React.FC = () => {
       <Button type="submit">Cadastrar</Button>
     </Form>
 
-    <a href="login">
+    <Link to="/">
       <FiArrowLeft />
       Voltar para logon
-    </a>
+    </Link>
   </Content>
   </Container>
   );
