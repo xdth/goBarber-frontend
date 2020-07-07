ac01c2d2b63ad45560493350a3dbac02295190c8
dth <dth@dthlabs.com>
2020-07-07 20:12:54 +0200

    animation browsing through pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -15,7 +15,7 @@ import logoImg from '../../assets/logo.svg'
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -52,6 +52,8 @@ const SignIn: React.FC = () => {
         const errors = getValidationErrors(err);
 
         formRef.current?.setErrors(errors);
+
+        return;
       }
 
       addToast({
@@ -65,23 +67,25 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail"/>
+            <Input name="email" icon={FiMail} placeholder="E-mail"/>
 
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
 
-          <Button type="submit">Entrar</Button>
+            <Button type="submit">Entrar</Button>
 
-          <a href="/forgot">Esqueci minha senha</a>
-        </Form>
+            <a href="/forgot">Esqueci minha senha</a>
+          </Form>
 
-        <Link to="/signup">
-          <FiLogIn />
-          Criar conta
-        </Link>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
       </Content>
       <Background/>
     </Container>
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
@@ -10,16 +10,34 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
+
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  /*justify-content: center;*/
-  place-content: center;
-
+  justify-content: center;
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     /*margin: 80px 0;*/
@@ -45,7 +63,7 @@ export const Content = styled.div`
   }
 
   > a {
-      color: #F4EDE8;
+      color: #FF9000;
       display: block;
       margin-top: 24px;
       text-decoration: none;
@@ -59,7 +77,7 @@ export const Content = styled.div`
       }
 
       &:hover {
-        color: ${shade(0.2, '#F4EDE8')};
+        color: ${shade(0.2, '#FF9000')};
       }
   }
 `;
